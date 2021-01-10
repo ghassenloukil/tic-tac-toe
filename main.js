857f98921f3a757916b36bea1e1bc430d2b9c95c
ghassenloukil <gaston.loukil@gmail.com>
2021-01-10 20:44:31 +0100

    project steps

--- main.js
+++ main.js
@@ -2,27 +2,22 @@ var scoreX = 0;
 var scoreO = 0;
 var player;
 
-function changeBackground(){
-    var r = Math.ceil(Math.random()*94)+75;
-    var g = Math.ceil(Math.random()*100)+68;
-    var b = Math.ceil(Math.random()*70)+50;
-    document.body.style.backgroundColor = 'hsl('+r+','+g+'%,'+b+'%)';
-}
+
+
 
 function alert1(){
    
-   alert("My first JS game! Let's try! Thanks for like! :) :) ");
-   alert("After win/lose/draw click "+"Restart"+ "! And play again! :) ");
+   alert("Welcome to my game! Let's try! Thanks for like! :)  ");
+   alert("After win Or lose click "+"Restart"+ "! And play again! :) ");
 }
 
 function start(){
     choosePlayer();
     currentPlayer();
-    changeBackground();
     alert1();
 }
 
-setInterval(changeBackground, 7000);
+
 
 function choosePlayer() {
     player = Math.floor(Math.random() * 2);
