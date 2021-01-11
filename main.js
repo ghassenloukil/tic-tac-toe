14d6afdbeedaffa1553c0397295aae15faed87e1
ghassenloukil <gaston.loukil@gmail.com>
2021-01-11 13:40:51 +0100

    still working :D

--- main.js
+++ main.js
@@ -1,8 +1,11 @@
+var turn ;
 var scoreX = 0;
 var scoreO = 0;
 var player;
-
-
+var topLeft, middleLeft, bottomLeft;
+var topCenter, middleCenter, bottomCenter;
+var topRight, middleRight, bottomRight;
+var turn =document.getElementById('turn').innerHTML;
 
 
 function alert1(){
@@ -18,17 +21,17 @@ function start(){
 }
 
 
-var boolean=true
+
 function choosePlayer() {
     player = Math.floor(Math.random() * 2);
 }
 function currentPlayer() {
-    if (player === boolean) {
+    if (player === 1) {
         document.getElementById("playerStatus").innerHTML = "Current player: X";
-        boolean==!boolean
-      } else if (player === boolean) {
+      
+      } else if (player === 0) {
         document.getElementById("playerStatus").innerHTML = "Current player: O";
-        boolean===boolean
+      
           }
 }
 
@@ -36,11 +39,12 @@ window.onload = start;
 
 function game(btn) {
     if (player === 1) {
+        console.log('am here')
         currentPlayer();
         document.getElementById(btn).value = "X";
          
         document.getElementById(btn).disabled = "disabled";
-        winner();
+        // winner();
          player -= 1;
         currentPlayer();
 
@@ -49,10 +53,56 @@ function game(btn) {
         document.getElementById(btn).value = "O";
         
         document.getElementById(btn).disabled = "disabled";
-        winner();
+        // winner();
         player += 1;
         currentPlayer();
     }
 
 }
 
+function check(){
+    topLeft=document.getElementById("btn1")
+    middlLeft=document.getElementById("btn2")
+    bottomLeft=document.getElementById("btn3")
+
+    topCenter=document.getElementById("btn4")
+    middleCenter=document.getElementById("btn5")
+    bottomCenter=document.getElementById("btn6")
+
+    topRight=document.getElementById("btn7")
+    middleRight=document.getElementById("btn8")
+    bottomRight=document.getElementById("btn9")
+
+    if((topLeft==='X')&&(topCenter==='X')&&(topRight==='X')){
+        document.getElementById('turn').innerHTML = 'X WON!'
+                return ;
+        }
+        if((middlLeft==='X')&&(middleCenter==='x')&&(middleRight==='X')){
+            document.getElementById('turn').innerHTML ='X WON!'
+            return ;
+        }
+        if((bottomLeft==='X')&&(bottomCenter==='X')&&(bottomRight==='X')){
+            document.getElementById('turn').innerHTML ='X WON!'
+            return ;
+        }
+        if((topLeft==='X')&&(middlLeft==='X')&&(bottomLeft==='X')){
+            document.getElementById('turn').innerHTML='X WON !'
+            return ;
+        }
+        if ((topCenter==='X')&&(middleCenter==='X')&&(bottomCenter==='X')){
+            document.getElementById('turn').innerHTML ='X WON!'
+            return ;
+        }
+        if((topRight==='X')&&(middleRight==='X')&&(bottomRight==='X')){
+            document.getElementById('turn').innerHTML='X WON!'
+            return ;
+        }
+        if((topLeft==='X')&&(middleCenter==='X')&&(bottomRight==='X')){
+            document.getElementById('turn').innerHTML='X WON!'
+            return ;
+        }
+        if((bottomLeft==='X')&&(middleCenter==='X')&&(topRight==='X')){
+            document.getElementById('turn').innerHTML='X WON!'
+            return ;
+        }
+    }
